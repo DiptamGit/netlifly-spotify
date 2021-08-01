0a2e1eb86380bd1ec153310f6603731f64608ae9
Diptam Sarkar <diptamsarkar@gmail.com>
2021-07-31 22:51:43 -0500

    pre 90's finalised

--- js/datatable.js
+++ js/datatable.js
@@ -1,13 +1,29 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
 
+    $("#scatter").append('<iframe src="charts/pre/popular_danceability.html" width="100%" height="600" title="description"></iframe>');
 
-    // $('#metrics').click(function (e) { 
-    //     console.log($("#metrics option:selected" ).text())
-    // });
+    $('#metrics').change(function (e) {
 
-    $('#metrics').change(function (e) { 
-        console.log($("#metrics option:selected" ).text())
+        let selectedOption = $("#metrics option:selected" ).text();
+        if(selectedOption == "Danceability"){
+            $('#scatter').empty().append('<iframe src="charts/pre/popular_danceability.html" width="100%" height="600" title="description"></iframe>')
+        }
+        if(selectedOption == "Energy"){
+            $("#scatter").empty().append('<iframe src="charts/pre/popular_energy.html" width="100%" height="600" title="description"></iframe>');
+        }
+        if(selectedOption == "Acousticness"){
+            $("#scatter").empty().append('<iframe src="charts/pre/popular_acousticness.html" width="100%" height="600" title="description"></iframe>');
+        }
+        if(selectedOption == "Liveness"){
+            $("#scatter").empty().append('<iframe src="charts/pre/popular_liveness.html" width="100%" height="600" title="description"></iframe>');
+        }
+        if(selectedOption == "Valence"){
+            $("#scatter").empty().append('<iframe src="charts/pre/popular_valence.html" width="100%" height="600" title="description"></iframe>');
+        }
+        if(selectedOption == "Tempo"){
+            $('#scatter').empty().append('<iframe src="charts/pre/popular_tempo.html" width="100%" height="600" title="description"></iframe>')
+        }
         
     });
 
